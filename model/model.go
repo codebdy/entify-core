d7c4d7d647a14f55af4d650d978a362594a82e40
Water.Li <lizongbin@hotmail.com>
2023-05-09 16:01:32 +0800

    Update model.go

--- model/model.go
+++ model/model.go
@@ -1,6 +1,10 @@
 package model
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+
 	"github.com/codebdy/entify/model/domain"
 	"github.com/codebdy/entify/model/graph"
 	"github.com/codebdy/entify/model/meta"
@@ -25,3 +29,15 @@ func New(c *meta.UMLMeta, metaId uint64) *Model {
 }
 
 var SystemModel *Model
+
+func ReadContentFromJson(fileName string) meta.UMLMeta {
+	data, err := ioutil.ReadFile(fileName)
+	content := meta.UMLMeta{}
+	if nil != err {
+		log.Panic(err.Error())
+	} else {
+		err = json.Unmarshal(data, &content)
+	}
+
+	return content
+}
